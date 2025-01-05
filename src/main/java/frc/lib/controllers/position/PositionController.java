478ff3146559b6eed7632e2564000518433683b1
lddtwo456 <lddtwo45@gmail.com>
2025-01-05 04:57:28 -0500

    TalonFX position controller

--- src/main/java/frc/lib/controllers/position/PositionController.java
+++ src/main/java/frc/lib/controllers/position/PositionController.java
@@ -8,4 +8,47 @@ package frc.lib.controllers.position;
  */
 public interface PositionController {
   
+  /** Position controller values */
+  public static class PositionControllerValues {
+    
+    /** Current position in rotations */
+    public double posRotations = 0.0;
+
+    /** Current velocity in rotations per second */
+    public double velRotationsPerSec = 0.0;
+
+    /** Current accleration in rotations per second per second */
+    public double accRotationsPerSecPerSec = 0.0;
+
+    /** Current voltage */
+    public double motorVolts = 0.0;
+
+    /** Current current */
+    public double motorAmps = 0.0;
+  }
+
+  /** Configures the position controller */
+  public void configure();
+
+  /**
+   * Get the position controller's updated values and update the provided values class accordingly
+   * 
+   * @param values values class to be updated
+   */
+  public void getUpdatedVals(PositionControllerValues values);
+
+  /**
+   * Sets the position of the position controller
+   * 
+   * @param posRotations position in rotations
+   */
+  public void setPos(double posRotations);
+
+  /**
+   * Sets the setpoint (target position and velocity) of the controller
+   * 
+   * @param posRotations target position in rotations
+   * @param velRotationsPerSec target velocity in rotations per second
+   */
+  public void setSetpoint(double posRotations, double velRotationsPerSec);
 }
--- src/main/java/frc/lib/controllers/position/PositionControllerTalonFXSteer.java
+++ src/main/java/frc/lib/controllers/position/PositionControllerTalonFXSteer.java
@@ -1,6 +1,118 @@
 package frc.lib.controllers.position;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import frc.lib.CAN;
+import frc.lib.configs.MechanismConfig;
+import frc.lib.configs.appliers.CANcoderConfigApplier;
+import frc.lib.configs.appliers.TalonFXConfigApplier;
+
 /** TalonFX position controller used for steering on a swerve module */
 public class PositionControllerTalonFXSteer implements PositionController {
   
+  private final MechanismConfig config;
+
+  private final TalonFX motor;
+  
+  private final CANcoder azimuthEncoder;
+
+  private final StatusSignal<Angle> position;
+  private final StatusSignal<AngularVelocity> velocity;
+  private final StatusSignal<AngularAcceleration> acceleration;
+  private final StatusSignal<Voltage> volts;
+  private final StatusSignal<Current> amps;
+
+  private final SimpleMotorFeedforward feedforward;
+
+  private final PIDController feedback;
+
+  private final VoltageOut voltage;
+
+  public PositionControllerTalonFXSteer(
+      CAN steerCAN,
+      CAN encoderCAN,
+      MechanismConfig config,
+      boolean enableFOC) {
+    
+    this.config = config;
+
+    // create hardware
+    motor = new TalonFX(steerCAN.id(), steerCAN.bus());
+    azimuthEncoder = new CANcoder(encoderCAN.id(), encoderCAN.bus());
+
+    // status signals (values in memory updated at a fixed rate by hardware over CAN)
+    position = azimuthEncoder.getAbsolutePosition();
+    velocity = motor.getVelocity();
+    acceleration = motor.getAcceleration();
+    volts = motor.getMotorVoltage();
+    amps = motor.getStatorCurrent();
+
+    // create feedforward and feedback based on config
+    feedforward = config.feedforwardControllerConfig().createSimpleMotorFeedforward();
+    feedback = config.feedbackControllerConfig().createPIDController();
+
+    // default voltage
+    voltage = new VoltageOut(0.0).withEnableFOC(enableFOC);
+  }
+
+  @Override
+  public void configure() {
+    BaseStatusSignal.setUpdateFrequencyForAll(100, position, velocity, acceleration);
+    BaseStatusSignal.setUpdateFrequencyForAll(10, volts, amps);
+
+    ParentDevice.optimizeBusUtilizationForAll(motor, azimuthEncoder);
+
+    TalonFXConfigApplier.applyFactoryDefault(motor);
+    TalonFXConfigApplier.apply(motor, config.motorConfig());
+
+    CANcoderConfigApplier.applyFactoryDefault(azimuthEncoder);
+    CANcoderConfigApplier.apply(azimuthEncoder, config.absoluteEncoderConfig());
+  }
+
+  @Override
+  public void getUpdatedVals(PositionControllerValues values) {
+    values.posRotations = position.getValueAsDouble();
+    values.velRotationsPerSec = velocity.getValueAsDouble();
+    values.accRotationsPerSecPerSec = acceleration.getValueAsDouble();
+    values.motorVolts = volts.getValueAsDouble();
+    values.motorAmps = amps.getValueAsDouble();
+  }
+
+  @Override
+  public void setPos(double posRotations) {}
+
+  @Override
+  public void setSetpoint(double posRotations, double velRotationsPerSec) {
+    double measuredPosRotations = position.getValueAsDouble();
+
+    double feedforwardVolts = calculateFeedforward(measuredPosRotations, posRotations);
+    
+    double feedbackVolts = feedback.calculate(measuredPosRotations, posRotations);
+
+    motor.setControl(voltage.withOutput(feedforwardVolts + feedbackVolts));
+  }
+
+  private double calculateFeedforward(double measurementRotations, double setpointRotations) {
+    if (feedback.atSetpoint() == false) {
+      if (measurementRotations > setpointRotations) {
+        return feedforward.getKs();
+      } else {
+        return -feedforward.getKs();
+      }
+    }
+
+    return 0.0;
+  }
 }
