04952d3258e2061aa6f101be74e40cd85bf15c0f
lddtwo456 <lddtwo45@gmail.com>
2025-01-05 06:30:06 -0500

    pigeon 2 gyroscope

---
+++ src/main/java/frc/lib/sensors/Gyroscope.java
@@ -0,0 +1,49 @@
+package frc.lib.sensors;
+
+/**
+ * Basic interface for gyroscopes
+ * 
+ * Outlines required functions and variables all gyroscopes have to make implementing new types
+ * of gyroscopes in the future much simpler
+ */
+public interface Gyroscope {
+  
+  /** Gyroscope values */
+  public static class GyroscopeValues {
+
+    /** Roll angle in rotations */
+    public double rollRotations = 0.0;
+
+    /** Pitch angle in rotations */
+    public double pitchRotations = 0.0;
+
+    /** Yaw angle in rotations */
+    public double yawRotations = 0.0;
+
+    /** Roll angle velocity in rotations per second */
+    public double rollVelRotationsPerSec = 0.0;
+
+    /** Pitch angle velocity in rotations per second */
+    public double pitchVelRotationsPerSec = 0.0;
+
+    /** Yaw angle in rotations per second */
+    public double yawVelRotationsPerSec = 0.0;
+  }
+
+  /** Configures the gyroscope */
+  public void configure();
+
+  /**
+   * Get the gyroscope's updated values and update the provided values class accordingly
+   * 
+   * @param values values class to be updated
+   */
+  public void getUpdatedVals(GyroscopeValues values);
+
+  /**
+   * Sets the gyroscope's yaw in rotations
+   * 
+   * @param yawRotations yaw in rotations
+   */
+  public void setYaw(double yawRotations);
+}
---
+++ src/main/java/frc/lib/sensors/GyroscopePigeon2.java
@@ -0,0 +1,57 @@
+package frc.lib.sensors;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import frc.lib.CAN;
+import frc.lib.configs.appliers.Pigeon2ConfigApplier;
+
+/** Pigeon 2 gyroscope */
+public class GyroscopePigeon2 implements Gyroscope {
+  
+  private Pigeon2 gyroscope;
+
+  private final StatusSignal<Angle> roll, pitch, yaw;
+  private final StatusSignal<AngularVelocity> rollVelocity, pitchVelocity, yawVelocity;
+
+  public GyroscopePigeon2(
+      CAN gyroscopeCAN) {
+    
+    gyroscope = new Pigeon2(gyroscopeCAN.id(), gyroscopeCAN.bus());
+
+    roll = gyroscope.getRoll();
+    pitch = gyroscope.getPitch();
+    yaw = gyroscope.getYaw();
+
+    rollVelocity = gyroscope.getAngularVelocityXWorld();
+    pitchVelocity = gyroscope.getAngularVelocityYWorld();
+    yawVelocity = gyroscope.getAngularVelocityZWorld();
+  }
+
+  @Override
+  public void configure() {
+    BaseStatusSignal.setUpdateFrequencyForAll(100, yaw, yawVelocity);
+    BaseStatusSignal.setUpdateFrequencyForAll(10, roll, pitch, rollVelocity, pitchVelocity);
+
+    Pigeon2ConfigApplier.applyFactoryDefault(gyroscope);
+  }
+
+  @Override
+  public void getUpdatedVals(GyroscopeValues values) {
+    values.rollRotations = Units.degreesToRotations(roll.getValueAsDouble());
+    values.pitchRotations = Units.degreesToRotations(pitch.getValueAsDouble());
+    values.yawRotations = Units.degreesToRotations(yaw.getValueAsDouble());
+    values.rollVelRotationsPerSec = Units.degreesToRotations(rollVelocity.getValueAsDouble());
+    values.pitchVelRotationsPerSec = Units.degreesToRotations(pitchVelocity.getValueAsDouble());
+    values.yawVelRotationsPerSec = Units.degreesToRotations(yawVelocity.getValueAsDouble());
+  }
+
+  @Override
+  public void setYaw(double yawRotations) {
+    gyroscope.setYaw(Units.rotationsToDegrees(yawRotations));
+  }
+}
