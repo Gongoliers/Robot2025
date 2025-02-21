fcc58314889a0eb35a755f896cb1e191a0cd2f08
lddtwo456 <lddtwo45@gmail.com>
2025-02-21 03:29:41 -0500

    better safe movement of elevator?

--- src/main/java/frc/robot/manipulator/PivotState.java
+++ src/main/java/frc/robot/manipulator/PivotState.java
@@ -1,9 +1,11 @@
 package frc.robot.manipulator;
 
 public enum PivotState {
-  STOW(0.33203125, 0, true),
-  SAFE(0.2, 0, false),
-  ALGAE(-0.1, 0, false);
+  STOW(0.33203125, 0),
+  SAFE(0.2, 0),
+  SCORE(0.1, 0),
+  ALGAE(-0.1, 0),
+  FLOOR(-0.35, 0);
 
   private final double posRotations;
   private final double velRotationsPerSec;
