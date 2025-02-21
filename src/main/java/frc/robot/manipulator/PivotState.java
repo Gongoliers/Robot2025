4d3080dc66d8131eee313a21b4ab27938a8becda
lddtwo456 <lddtwo45@gmail.com>
2025-02-21 03:06:23 -0500

    add isUnsafe bool to pivot states

--- src/main/java/frc/robot/manipulator/PivotState.java
+++ src/main/java/frc/robot/manipulator/PivotState.java
@@ -1,20 +1,22 @@
 package frc.robot.manipulator;
 
 public enum PivotState {
-  STOW(0.33203125, 0),
-  SAFE(0.2, 0),
-  SCORE(0.1, 0),
-  ALGAE(-0.1, 0);
+  STOW(0.33203125, 0, true),
+  SAFE(0.2, 0, false),
+  ALGAE(-0.1, 0, false);
 
   private final double posRotations;
   private final double velRotationsPerSec;
+  private final boolean isUnsafe;
   
   PivotState(
       double posRotations,
-      double velRotationsPerSec) {
+      double velRotationsPerSec,
+      boolean isUnsafe) {
     
     this.posRotations = posRotations;
     this.velRotationsPerSec = velRotationsPerSec;
+    this.isUnsafe = isUnsafe;
   }
 
   public double getPosRotations() {
@@ -23,5 +25,9 @@ public enum PivotState {
 
   public double getVelRotationsPerSec() {
     return velRotationsPerSec;
+  }
+
+  public boolean isUnsafe()  {
+    return isUnsafe;
   }
 }
