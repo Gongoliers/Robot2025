4712194dece6034423d2e671954238ddf7c0e3d4
Hayden Heroux <haydenheroux@gmail.com>
2025-11-12 14:48:50 -0500

    Demo of elevator simulation using MotorOutputSim.

--- src/main/java/frc/lib/motors/MotorOutputSim.java
+++ src/main/java/frc/lib/motors/MotorOutputSim.java
@@ -1,63 +1,89 @@
 package frc.lib.motors;
 
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N2;
-import edu.wpi.first.math.system.LinearSystem;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
+import java.util.function.Supplier;
+
 import static edu.wpi.first.units.Units.*;
 
-public class MotorOutputSim extends DCMotorSim implements MotorOutput {
+public class MotorOutputSim implements MotorOutput {
+
+    private final DCMotorSim sim;
 
     private final Voltage kS;
 
+    private final Supplier<Voltage> kG;
+
     /**
-     * @param system  The system to simulate, created by {@link LinearSystemId}.
-     * @param gearbox The type of and number of motors in the system.
-     * @param kS      The voltage loss due to static friction.
+     * @param sim The motor system to simulate.
+     * @param kS  The voltage loss due to static friction.
+     * @param kG  The voltage loss due to gravity.
      */
-    public MotorOutputSim(LinearSystem<N2, N1, N2> system, DCMotor gearbox, Voltage kS) {
-        super(system, gearbox);
+    public MotorOutputSim(DCMotorSim sim, Voltage kS, Supplier<Voltage> kG) {
+        this.sim = sim;
         this.kS = kS;
+        this.kG = kG;
+    }
+
+    /**
+     * @param sim The motor system to simulate.
+     * @param kS  The voltage loss due to static friction.
+     * @param kG  The voltage loss due to gravity.
+     */
+    public MotorOutputSim(DCMotorSim sim, Voltage kS, Voltage kG) {
+        this(sim, kS, () -> kG);
+    }
+
+    /**
+     * @param sim The motor system to simulate.
+     * @param kS  The voltage loss due to static friction.
+     */
+    public MotorOutputSim(DCMotorSim sim, Voltage kS) {
+        this(sim, kS, Volts::zero);
+    }
+
+    /**
+     * @param sim The motor system to simulate.
+     */
+    public MotorOutputSim(DCMotorSim sim) {
+        this(sim, Volts.zero());
     }
 
     @Override
     public void setVoltage(Voltage voltage) {
-        double effectiveVoltage = calculateEffectiveVoltage(voltage.in(Volts));
-        setInputVoltage(effectiveVoltage);
+        double volts = voltage.in(Volts);
+        sim.setInputVoltage(volts - calculateVoltageLoss(volts));
     }
 
-    private double calculateEffectiveVoltage(double voltage) {
+    private double calculateVoltageLoss(double voltage) {
         double kS = this.kS.in(Volts);
+        double kG = this.kG.get().in(Volts);
         if (Math.abs(voltage) < kS) {
-            return 0;
+            return kG;
         }
-        double opposingVoltage = Math.copySign(kS, -voltage);
-        return voltage + opposingVoltage;
+        return Math.copySign(kS, voltage) + kG;
     }
 
     @Override
     public void updateValues(MotorValues values, Time dt) {
-        update(dt.in(Seconds));
+        sim.update(dt.in(Seconds));
 
-        double motorVoltage = getInputVoltage();
+        double motorVoltage = sim.getInputVoltage();
         double supplyVoltage = RobotController.getBatteryVoltage();
         double dutyCycle = motorVoltage / supplyVoltage;
-        double statorCurrent = getCurrentDrawAmps();
+        double statorCurrent = sim.getCurrentDrawAmps();
         double supplyCurrent = statorCurrent * dutyCycle;
 
-        values.position.mut_replace(getAngularPositionRad(), Radian);
+        values.position.mut_replace(sim.getAngularPositionRad(), Radian);
         values.velocity.mut_replace(
-                getAngularVelocityRadPerSec(),
+                sim.getAngularVelocityRadPerSec(),
                 RadiansPerSecond
         );
         values.acceleration.mut_replace(
-                getAngularAccelerationRadPerSecSq(),
+                sim.getAngularAccelerationRadPerSecSq(),
                 RadiansPerSecondPerSecond
         );
         values.motorVoltage.mut_replace(motorVoltage, Volts);
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,22 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Meters;
-
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib.Telemetry;
+import frc.lib.motors.MotorOutputSim;
+import frc.lib.motors.MotorValues;
 import frc.robot.elevator.Elevator;
 import frc.robot.elevator.ElevatorState;
 import frc.robot.pivot.Pivot;
+
+import static edu.wpi.first.units.Units.*;
 
 /** Robot container */
 public class RobotContainer {
@@ -35,6 +42,19 @@ public class RobotContainer {
   /** Pivot subsystem reference */
   private final Pivot pivot;
 
+    /**
+     * System identification constants, for simulation.
+     */
+    private final double kS = 0.12;
+    private final double kG = 0.575;
+    private final double kV = 0.1;
+    private final double kA = 0.09;
+    private final DCMotor gearbox = DCMotor.getKrakenX60(2);
+
+    private final MotorOutputSim sim;
+
+    private final ElevatorFeedforward feedforward;
+
   /** Initializes the robot container */
   private RobotContainer() {
     driverController = new CommandXboxController(0);
@@ -43,6 +63,10 @@ public class RobotContainer {
     elevator = Elevator.getInstance();
 
     pivot = Pivot.getInstance();
+
+      var motor = new DCMotorSim(LinearSystemId.identifyPositionSystem(kV, kA), gearbox);
+      sim = new MotorOutputSim(motor, Volts.of(kS), Volts.of(kG));
+      feedforward = new ElevatorFeedforward(kS, kG, kV, kA);
 
     Telemetry.initializeTabs(elevator, pivot);
 
@@ -80,10 +104,14 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    if (RobotConstants.ENABLED_SUBSYSTEMS.contains(RobotConstants.Subsystem.AUTO)) {
-      ;
-    }
+      var goalSpeed = RadiansPerSecond.of(10);
+      MotorValues values = new MotorValues();
 
-    return Commands.print("Auto disabled");
+      return Commands.run(() -> {
+          sim.setVoltage(Volts.of(feedforward.calculate(goalSpeed.in(RadiansPerSecond))));
+          sim.updateValues(values, Seconds.of(0.02));
+          SmartDashboard.putNumber("Simulated Position", values.position.in(Radians));
+          SmartDashboard.putNumber("Simulated Speed", values.velocity.in(RadiansPerSecond));
+    });
   }
 }
