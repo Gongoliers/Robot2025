c3cf608e8360f63b19c83e89ccfdf27a00cd9ce2
Hayden Heroux <haydenheroux@gmail.com>
2025-11-14 19:38:50 -0500

    Use MotorOutputSim for elevator.

--- src/main/java/frc/robot/elevator/ElevatorFactory.java
+++ src/main/java/frc/robot/elevator/ElevatorFactory.java
@@ -1,15 +1,15 @@
 package frc.robot.elevator;
 
-import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
 import frc.lib.CAN;
 import frc.lib.configs.MechanismConfig;
-import frc.lib.controllers.position.PositionController;
-import frc.lib.controllers.position.PositionControllerSim;
-import frc.lib.controllers.position.PositionControllerTalonFXElevator;
 import frc.lib.motors.MotorOutput;
-import frc.lib.motors.MotorOutputFlywheelSim;
+import frc.lib.motors.MotorOutputSim;
 import frc.lib.motors.MotorOutputTalonFX2;
 import frc.robot.Robot;
 import frc.robot.RobotConstants;
@@ -17,26 +17,25 @@ import frc.robot.RobotConstants.Subsystem;
 
 /** Creates elevator hardware */
 public class ElevatorFactory {
-  
-  /** 
-   * Creates an elevator position controller 
-   * 
-   * @return an elevator position controller
-   */
-  public static MotorOutput createMotorOutput(MechanismConfig config) {
-    if (Robot.isReal() && RobotConstants.ENABLED_SUBSYSTEMS.contains(Subsystem.ELEVATOR)) {
-      return new MotorOutputTalonFX2(
-        config.motorConfig(), 
-        new CAN(10), 
-        new CAN(11), 
-        false);
-    }
 
-    //TODO: this is a temporary sim that is not at all usable
-    return new MotorOutputFlywheelSim(
-      config.motorConfig(), 
-      new DCMotor(1, 1, 1, 1, 1, 1),
-      KilogramSquareMeters.of(1.0),
-      1.0);
-  }
+    /**
+     * Creates an elevator position controller
+     *
+     * @return an elevator position controller
+     */
+    public static MotorOutput createMotorOutput(MechanismConfig config) {
+        if (Robot.isReal() && RobotConstants.ENABLED_SUBSYSTEMS.contains(Subsystem.ELEVATOR)) {
+            return new MotorOutputTalonFX2(config.motorConfig(), new CAN(10), new CAN(11), false);
+        }
+
+        var plant =
+                LinearSystemId.identifyPositionSystem(
+                        config.feedforwardControllerConfig().kV(),
+                        config.feedforwardControllerConfig().kA());
+        var sim = new DCMotorSim(plant, DCMotor.getKrakenX60(2));
+        return new MotorOutputSim(
+                sim,
+                Volts.of(config.feedforwardControllerConfig().kS()),
+                Volts.of(config.feedforwardControllerConfig().kG()));
+    }
 }
