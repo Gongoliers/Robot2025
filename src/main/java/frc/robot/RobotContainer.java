e38df912977dea6a7bdf2e550089d3683ec5cc50
Hayden Heroux <haydenheroux@gmail.com>
2025-11-17 16:58:12 -0500

    Soft path to pose.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,11 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Meters;
-
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,6 +19,8 @@ import frc.lib.swerves.IdealSwerveSim;
 import frc.robot.elevator.Elevator;
 import frc.robot.elevator.ElevatorState;
 import frc.robot.pivot.Pivot;
+
+import static edu.wpi.first.units.Units.*;
 
 /** Robot container */
 public class RobotContainer {
@@ -85,15 +87,50 @@ public class RobotContainer {
     operatorController.y().onTrue(elevator.setElevatorPosition(Meters.of(0)));
   }
 
+  public Translation2d mixedVelocity(Translation2d driverVelocity, Pose2d current, Pose2d target) {
+    final double KP = 4;
+    final Distance MAX_DISTANCE = Meters.of(1);
+    final Distance MIN_DISTANCE = Feet.of(1);
+
+    Translation2d error = new Translation2d(target.getX() - current.getX(), target.getY() - current.getY());
+    Rotation2d direction = error.getAngle();
+    double distance = error.getNorm();
+    SmartDashboard.putNumber("Distance (m)", distance);
+
+    if (distance > MAX_DISTANCE.in(Meters)) {
+        return driverVelocity;
+    }
+
+    double velocity = KP * distance;
+    double x = velocity * direction.getCos();
+    double y = velocity * direction.getSin();
+    Translation2d proportionalVelocity = new Translation2d(x, y);
+
+    if (distance < MIN_DISTANCE.in(Meters)) {
+        return proportionalVelocity;
+    }
+
+    double t = distance / MAX_DISTANCE.in(Meters);
+    SmartDashboard.putNumber("t", t);
+    Translation2d scaledDriverVelocity = driverVelocity.times(t);
+    Translation2d scaledProportionalVelocity = proportionalVelocity.times(1 - t);
+
+    return scaledDriverVelocity.plus(scaledProportionalVelocity);
+  }
+
   public Command getAutonomousCommand() {
     IdealSwerveSim swerve = new IdealSwerveSim();
-    SwerveRequest request = new SwerveRequest.FieldCentricFacingAngle().withVelocityX(0.1).withTargetDirection(Rotation2d.k180deg);
+    SwerveRequest.FieldCentricFacingAngle request = new SwerveRequest.FieldCentricFacingAngle();
     Field2d field = new Field2d();
     SmartDashboard.putData(field);
+    Pose2d target = new Pose2d(Inches.of(144).in(Meters), Inches.of(153.5).in(Meters), Rotation2d.kZero);
+    Translation2d driverVelocity = new Translation2d(MetersPerSecond.of(3).in(MetersPerSecond), MetersPerSecond.of(3).in(MetersPerSecond));
 
     return Commands.run(() -> {
-        swerve.setControl(request);
-        field.setRobotPose(swerve.getState().Pose);
+        var pose = swerve.getState().Pose;
+        field.setRobotPose(pose);
+        var velocity = mixedVelocity(driverVelocity, pose, target);
+        swerve.setControl(request.withVelocityX(velocity.getX()).withVelocityY(velocity.getY()));
     });
   }
 }
