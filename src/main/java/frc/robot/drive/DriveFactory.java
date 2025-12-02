647ab62a73d6fa776a4272095454e5334983c6f7
Hayden Heroux <haydenheroux@gmail.com>
2025-12-02 09:19:07 -0500

    Integrate generated code for the CTRE swerve library.

--- src/main/java/frc/robot/drive/DriveFactory.java
+++ src/main/java/frc/robot/drive/DriveFactory.java
@@ -7,14 +7,20 @@ import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.lib.Subsystem;
-import frc.lib.swerves.IdealSwerveSim;
+import frc.lib.swerves.PhoenixSwerve;
 import frc.lib.swerves.SwerveOutput;
 
 public class DriveFactory {
 
-  public static SwerveOutput createSwerve() {
-    return new IdealSwerveSim();
-  }
+    public static SwerveOutput createSwerve() {
+        return new PhoenixSwerve(
+                TunerConstants.DrivetrainConstants,
+                TunerConstants.FrontLeft,
+                TunerConstants.FrontRight,
+                TunerConstants.BackLeft,
+                TunerConstants.BackRight
+        );
+    }
 
   protected static SysIdRoutine createDriveRoutine(SwerveOutput swerve, Subsystem subsystem) {
     SwerveRequest.SysIdSwerveTranslation m_translationCharacterization =
