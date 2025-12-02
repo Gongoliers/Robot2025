082e3ba6229b4984f974cb51f774b5a4e644d064
Hayden Heroux <haydenheroux@gmail.com>
2025-12-02 09:14:59 -0500

    Simulated driving with driver controller.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,20 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Meters;
-
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib.Telemetry;
+import frc.robot.drive.Drive;
+import frc.robot.drive.DriveFactory;
 import frc.robot.elevator.Elevator;
 import frc.robot.elevator.ElevatorState;
 import frc.robot.pivot.Pivot;
+import static edu.wpi.first.units.Units.*;
 
 /** Robot container */
 public class RobotContainer {
@@ -35,6 +40,8 @@ public class RobotContainer {
   /** Pivot subsystem reference */
   private final Pivot pivot;
 
+  private final Drive drive;
+
   /** Initializes the robot container */
   private RobotContainer() {
     driverController = new CommandXboxController(0);
@@ -44,7 +51,9 @@ public class RobotContainer {
 
     pivot = Pivot.getInstance();
 
-    Telemetry.initializeTabs(elevator, pivot);
+    drive = new Drive(DriveFactory.createSwerve());
+
+    Telemetry.initializeTabs(elevator, pivot, drive);
 
     multithreader = Multithreader.getInstance();
     multithreader.start();
@@ -66,8 +75,23 @@ public class RobotContainer {
     return instance;
   }
 
+  private ChassisSpeeds getFieldSpeeds() {
+      LinearVelocity MAX_VELOCITY = MetersPerSecond.of(2);
+      AngularVelocity MAX_ANGULAR_VELOCITY = RotationsPerSecond.of(0.5);
+      var x = MathUtil.applyDeadband(-driverController.getLeftY(), 0.1);
+      var y = MathUtil.applyDeadband(-driverController.getLeftX(), 0.1);
+      var omega = MathUtil.applyDeadband(-driverController.getRightX(), 0.1);
+      return new ChassisSpeeds(
+          MAX_VELOCITY.times(x),
+          MAX_VELOCITY.times(y),
+          MAX_ANGULAR_VELOCITY.times(omega)
+      );
+  }
+
   /** Configures subsystem default commands for teleop */
-  public void configureDefaultCommands() {}
+  public void configureDefaultCommands() {
+      drive.setDefaultCommand(drive.drive(this::getFieldSpeeds));
+  }
 
   /** Configures controller bindings */
   private void configureBindings() {
@@ -79,10 +103,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    if (RobotConstants.ENABLED_SUBSYSTEMS.contains(RobotConstants.Subsystem.AUTO)) {
-      ;
-    }
-
-    return Commands.print("Auto disabled");
+      return Commands.print("No autonomous command selected...");
   }
 }
