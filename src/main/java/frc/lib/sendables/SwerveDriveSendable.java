3801bb730e7f7e64b9bb9ded8c079c5fcf82d6e8
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2025-12-10 12:57:58 -0500

    SwerveDriveSendable

---
+++ src/main/java/frc/lib/sendables/SwerveDriveSendable.java
@@ -0,0 +1,52 @@
+package frc.lib.sendables;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.drive.Drive;
+
+/** Swerve drive state sendable that allows fancy visualization in most dashboards and simpler looking swerve logging */
+public class SwerveDriveSendable implements Sendable {
+
+  /** Gets swerve module states [NW, NE, SW, SE] */
+  private final Supplier<SwerveModuleState[]> statesSupplier;
+
+  /** Gets robot rotation */
+  private final Supplier<Rotation2d> robotAngleSupplier;
+  
+  /**
+   * Constructs a swerve drive state sendable given a module states supplier and robot angle supplier
+   * 
+   * @param statesSupplier module states supplier [NW, NE, SW, SE]
+   * @param robotAngleSupplier robot angle supplier
+   */
+  public SwerveDriveSendable(
+      Supplier<SwerveModuleState[]> statesSupplier,
+      Supplier<Rotation2d> robotAngleSupplier) {
+    
+    this.statesSupplier = statesSupplier;
+    this.robotAngleSupplier = robotAngleSupplier;
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    builder.setSmartDashboardType("SwerveDrive");
+
+    builder.addDoubleProperty("Front Left Angle", () -> statesSupplier.get()[0].angle.getDegrees(), null);
+    builder.addDoubleProperty("Front Left Velocity", () -> statesSupplier.get()[0].speedMetersPerSecond, null);
+
+    builder.addDoubleProperty("Front Right Angle", () -> statesSupplier.get()[1].angle.getDegrees(), null);
+    builder.addDoubleProperty("Front Right Velocity", () -> statesSupplier.get()[1].speedMetersPerSecond, null);
+
+    builder.addDoubleProperty("Back Left Angle", () -> statesSupplier.get()[2].angle.getDegrees(), null);
+    builder.addDoubleProperty("Back Left Velocity", () -> statesSupplier.get()[2].speedMetersPerSecond, null);
+
+    builder.addDoubleProperty("Back Right Angle", () -> statesSupplier.get()[3].angle.getDegrees(), null);
+    builder.addDoubleProperty("Back Right Velocity", () -> statesSupplier.get()[3].speedMetersPerSecond, null);
+
+    builder.addDoubleProperty("Robot Angle", () -> robotAngleSupplier.get().getDegrees(), null);
+  }
+}
--- src/main/java/frc/robot/drive/Drive.java
+++ src/main/java/frc/robot/drive/Drive.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.lib.Subsystem;
+import frc.lib.sendables.SwerveDriveSendable;
 import frc.lib.swerves.SwerveOutput;
 import java.util.function.Supplier;
 
@@ -36,39 +37,16 @@ public class Drive extends Subsystem {
   public void initializeTab() {
     ShuffleboardTab tab = Shuffleboard.getTab("Swerve");
 
-    tab.add(field);
-    tab.addDoubleArray(
-        "States",
-        () -> {
-          SwerveModuleState[] states = state.ModuleStates;
-          double[] doubles = new double[8];
-
-          if (states != null) {
-            for (int i = 0; i < 4; i++) {
-              SwerveModuleState state = states[i];
-              doubles[2 * i] = state.angle.getDegrees();
-              doubles[2 * i + 1] = state.speedMetersPerSecond;
-            }
-          }
-
-          return doubles;
-        });
-    tab.addDoubleArray(
-        "Targets",
-        () -> {
-          SwerveModuleState[] states = state.ModuleTargets;
-          double[] doubles = new double[8];
-
-          if (states != null) {
-            for (int i = 0; i < 4; i++) {
-              SwerveModuleState state = states[i];
-              doubles[2 * i] = state.angle.getDegrees();
-              doubles[2 * i + 1] = state.speedMetersPerSecond;
-            }
-          }
-
-          return doubles;
-        });
+    tab.add(
+      "States", 
+      new SwerveDriveSendable(
+        () -> state.ModuleStates, 
+        () -> this.getPose().getRotation()));
+    tab.add(
+      "Targets",
+      new SwerveDriveSendable(
+        () -> state.ModuleTargets, 
+        () -> this.getPose().getRotation()));
   }
 
   @Override
