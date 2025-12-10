ca6f6fb7d29a2a1eac4b149a8ce9e261df20b8cb
Luke Davis <lddtwo45@gmail.com>
2025-12-09 19:44:17 -0500

    Format.

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -35,7 +35,5 @@ public class RobotConstants {
   }
 
   /** Enabled subsystems */
-  public static final Set<Subsystem> ENABLED_SUBSYSTEMS =
-      EnumSet.of(
-          Subsystem.SWERVE);
+  public static final Set<Subsystem> ENABLED_SUBSYSTEMS = EnumSet.of(Subsystem.SWERVE);
 }
